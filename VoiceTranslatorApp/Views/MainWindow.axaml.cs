9a88f77f94b7e72fbd747ff5c143a52a119b10aa
IgrokVV <yuraevd17042006@gmail.com>
2026-04-24 17:17:12 +0400

    Добавлен интерфейс

--- VoiceTranslatorApp/Views/MainWindow.axaml.cs
+++ VoiceTranslatorApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace VoiceTranslatorApp.Views
 {
@@ -7,6 +9,28 @@ namespace VoiceTranslatorApp.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ShowVoiceModels(object? sender, RoutedEventArgs e)
+        {
+            VoiceModelsPanel.IsVisible = true;
+            TranslationPanel.IsVisible = false;
+
+            VoiceModelsButton.Background = new SolidColorBrush(Color.Parse("#2563EB"));
+            VoiceModelsButton.Foreground = Brushes.White;
+            TranslationButton.Background = new SolidColorBrush(Color.Parse("#1E293B"));
+            TranslationButton.Foreground = new SolidColorBrush(Color.Parse("#E2E8F0"));
+        }
+
+        private void ShowTranslation(object? sender, RoutedEventArgs e)
+        {
+            VoiceModelsPanel.IsVisible = false;
+            TranslationPanel.IsVisible = true;
+
+            TranslationButton.Background = new SolidColorBrush(Color.Parse("#2563EB"));
+            TranslationButton.Foreground = Brushes.White;
+            VoiceModelsButton.Background = new SolidColorBrush(Color.Parse("#1E293B"));
+            VoiceModelsButton.Foreground = new SolidColorBrush(Color.Parse("#E2E8F0"));
         }
     }
 }
